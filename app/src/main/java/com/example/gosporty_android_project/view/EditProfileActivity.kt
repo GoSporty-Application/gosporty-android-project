9b2b26db1fb5ed2b5aa615549053cf08e49ac111
Santiago Gutierrez <santicoguvi02@gmail.com>
2023-06-09 14:13:02 -0500

    Adding the gallery option to change the photo

--- app/src/main/java/com/example/gosporty_android_project/view/EditProfileActivity.kt
+++ app/src/main/java/com/example/gosporty_android_project/view/EditProfileActivity.kt
@@ -5,7 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import com.example.gosporty_android_project.databinding.ActivityEditProfileBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.util.UUID
 
 class EditProfileActivity : AppCompatActivity() {
     val binding by lazy {
@@ -14,6 +21,9 @@ class EditProfileActivity : AppCompatActivity() {
     val prefRepository: PrefRepository by lazy {
         PrefRepository(this)
     }
+
+    private lateinit var galleryLauncher : ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -24,6 +34,14 @@ class EditProfileActivity : AppCompatActivity() {
         binding.epNombreTV.text = user.name
         binding.epNameET.text = Editable.Factory.getInstance().newEditable(user.name)
         binding.epUsernameET.text = Editable.Factory.getInstance().newEditable(user.username)
+
+        galleryLauncher = registerForActivityResult(StartActivityForResult(),::onGalleryResult)
+
+        binding.epEditPhotoIB.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            galleryLauncher.launch(intent)
+        }
 
         binding.epSoccerCB.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
@@ -48,6 +66,16 @@ class EditProfileActivity : AppCompatActivity() {
         binding.epSaveBTN.setOnClickListener {
             val intent: Intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    fun onGalleryResult(result : ActivityResult){
+        if(result.resultCode== RESULT_OK){
+            val uri = result.data?.data
+            binding.epPhotoIV.setImageURI(uri)
+            val filename = UUID.randomUUID().toString()
+            Firebase.storage.getReference().child("users").child(prefRepository.getUser().id!!).child(filename).putFile(uri!!)
+            Firebase.firestore.collection("users").document(prefRepository.getUser().id!!).update("photoUrl", filename)
         }
     }
 }
